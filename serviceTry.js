094a1d00001ca8a34bc6ccffe9c5c3e7648cab2d
codecrowk <137356213+codecrowk@users.noreply.github.com>
2024-01-19 18:59:37 -0500

    Add files via upload

--- serviceTry.js
+++ serviceTry.js
@@ -1,5 +1,6 @@
 const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
+    createElementVerification("Happy work david")
     try {
       const registration = await navigator.serviceWorker.register("/sw.js", {
         scope: "/",
@@ -16,9 +17,16 @@ const registerServiceWorker = async () => {
     }
   } else {
     console.log("no baby")
+    createElementVerification("Service worker no avalible")
   }
 };
 console.log("In a file")
+const createElementVerification = (elementValue) => {
+  const parent = document.getElementById("makingTest");
+  const makeElement = document.createElement("p");
+  makeElement.innerText = elementValue
+  parent.appendChild(makeElement)
+}
 // …
 
 registerServiceWorker();
