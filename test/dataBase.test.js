0d5073bd013f15729e3ab9b1aba18ba215f2db76
codecrowk <handres41@outlook.com>
2024-02-02 13:37:39 -0500

    update index links

--- test/dataBase.test.js
+++ test/dataBase.test.js
@@ -1,11 +1,10 @@
-// export {UserDataModel, ServicesDataModel, FollowDataModel}
 const {UserDataModel, FollowDataModel} = require('../script/data_base_model.js');
 const UserData = new UserDataModel;
 const FollowData = new FollowDataModel;
 
 test('Get user data', async () => {
   let data = await UserData.getUserByID("53cc")
-  expect(data).toMatchObject({"about": "changing user About", "account_created": "01/09/2020", "badges": ["commentor", "traveler", "landscaper"], "birthday": "21/01/1990", "credibility": 9.7, "email": "jesusRamiroLondono@gmail.com", "enrolled_events": ["200102020301", "302112032"], "follower_counter": 2100, "following": 120, "id": "53cc", "last_login": "21/04/2024", "level": "Guide", "level_points": 600, "name": "hola mundo", "nickname": "joseLuis21", "parche_counter": 7, "parche_points": 120, "password": "mySecurePassword", "post_counter": 3, "profile_photo": "joseLuis21_image.jpg", "status": "Desparchadisimo"});
+  expect(data).toMatchObject({"about": "changing user About", "account_created": "01/09/2020", "badges": ["commentor", "traveler", "landscaper"], "birthday": "21/01/1990", "credibility": 9.7, "email": "jesusRamiroLondono@gmail.com", "enrolled_events": ["200102020301", "302112032"], "follower_counter": 2100, "following": 120, "id": "53cc", "last_login": "21/04/2024", "level": "Guide", "level_points": 600, "name": "holamundo", "nickname": "holamundo", "parche_counter": 7, "parche_points": 120, "password": "mySecurePassword", "post_counter": 3, "profile_photo": "joseLuis21_image.jpg", "status": "Desparchadisimo"});
 });
 // Create user that doesn't exit
 test('Add following - user that does not exit', async () => {
