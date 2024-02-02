f9168638026df7c80ddbbec4bd42ca1a6672c857
codecrowk <handres41@outlook.com>
2024-02-02 13:45:48 -0500

    edit explorer star making nav js

---
+++ script/bottom_nav.js
@@ -0,0 +1,27 @@
+<nav id="menu-bot">
+
+  <a href="../index.html" class="boxes-menu-bot box-home">
+    <i class="fa-solid fa-house"></i>
+    <span>home</span>
+  </a>
+
+  <a href="explore.html" class="boxes-menu-bot box-explore">
+    <i class="fa-solid fa-house"></i>
+    <span>explore</span>
+  </a>
+
+  <a href="post.html" class="boxes-menu-bot box-publicar">
+    <i class="fa-solid fa-house"> <span>POST</span></i>
+  </a>
+
+  <a href="mesages.html" class="boxes-menu-bot box-post">
+    <i class="fa-solid fa-house"></i>
+    <span>notifications</span>
+  </a>
+
+  <a href="me.html" class="boxes-menu-bot box-me">
+    <i class="fa-solid fa-user"></i>
+    <span>me</span>
+  </a>
+
+</nav>
