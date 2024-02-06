b217091d5f3ceb717599515e9ef72ad715a2b9e2
riwip5-007 <luisapantojam28@gmail.comgit config --global user.nameluisapantojam28>
2024-02-05 19:58:27 -0500

    muy jodido

--- script/active_session.js
+++ script/active_session.js
@@ -1,6 +1,6 @@
 // User token will be user ID (for now)
-const userLog = localStorage.getItem("userToken");
-const isUserLoged = Boolean(userLog);
+const userLog = localStorage.getItem('userToken');
+const isUserLoged = Boolean(userLog)
 if (!isUserLoged) {
   window.location.href = "./views/login.html";
 }
